388b91090c917eeb0ea5131e4765407eaeab9a63
Danton <dantyt337@gmail.com>
2024-08-07 11:27:08 -0300

    Atualizando App.css e App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+export default function App(){
+  const  [ nutri , setNutri ] = useState([]);
+
+  useEffect(()=>{
+    let url = 'https://sujeitoprogramador.com/rn-api/?api=posts';
+
+    fetch(url)
+    .then((r) => r.json()) //Conversão para arquivo json (forçar o arquivo ser json)
+    .then((json) => {
+      setNutri(json)
+    })
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <strong>React Nutri</strong>
       </header>
+
+      <div>
+        {nutri.map((item)=>{
+          return(
+            <article key={item.id}>
+              <strong>{item.titulo}</strong>
+              <img src={item.capa} alt={item.titulo}></img>
+              <p>{item.paragrafo}</p>
+              <span>{item.categoria}</span>
+            </article>
+          );
+        })}
+      </div>
     </div>
   );
 }
-
-export default App;
